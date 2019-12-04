6b370a71bfa8972a33c26c96619191026b35883a
Anton Melekhavets <melekhavets@scand.com>
2019-12-04 11:54:47 +0300

    Fixed loading of native libs

--- src/main/java/com/test/jfx/Extra/X11Extra.java
+++ src/main/java/com/test/jfx/Extra/X11Extra.java
@@ -4,5 +4,5 @@ import com.sun.jna.Native;
 import com.sun.jna.platform.unix.X11;
 
 public interface X11Extra extends X11 {
-    X11Extra INSTANCE = (X11Extra) Native.load("libX11", X11.class);
+    X11Extra INSTANCE = (X11Extra) Native.load("libX11", X11Extra.class);
 }
